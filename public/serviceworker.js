28302c16323f16c4791395f39575ec618b16c902
davidahiekpor <david.ahiekpor@dreamoval.com>
2023-12-26 12:32:14 +0000

    PWA testing IOS

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,42 +1,88 @@
-const CACHE_NAME = "version-1";
-const urlsToCache = ["index.html", "offline.html"];
+// const CACHE_NAME = "version-1";
+// const urlsToCache = ["index.html", "offline.html"];
 
-const self = this;
+// const self = this;
 
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       console.log("Opened cache");
+
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then(() => {
+//       return fetch(event.request).catch(() => caches.match("offline.html"));
+//     })
+//   );
+// });
+
+// self.addEventListener("activate", (event) => {
+//   const cacheWhitelist = [];
+//   cacheWhitelist.push(CACHE_NAME);
+
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) =>
+//       Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (!cacheWhitelist.includes(cacheName)) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       )
+//     )
+//   );
+// });
+const CACHE_NAME = "my-pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/styles/main.css",
+  "/scripts/main.js",
+  "index.html",
+  "offline.html",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
-
-      return cache.addAll(urlsToCache);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .catch((error) => console.log("Cache open failed:", error))
   );
 });
-
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match("offline.html"));
-    })
-  );
-});
+    caches
+      .match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
 
-
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [];
-  cacheWhitelist.push(CACHE_NAME);
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
+        return fetch(event.request).then((response) => {
+          if (
+            !response ||
+            response.status !== 200 ||
+            response.type !== "basic"
+          ) {
+            return response;
           }
-        })
-      )
-    )
+
+          const responseToCache = response.clone();
+
+          caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.put(event.request, responseToCache))
+            .catch((error) => console.error("Failed to cache:", error));
+
+          return response;
+        });
+      })
+      .catch((error) => console.error("Fetch failed:", error))
   );
 });
