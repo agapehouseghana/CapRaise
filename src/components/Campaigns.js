7540d233bc79b1a4a8a4109d9f30bc25ce2f4782
davidahiekpor <david.ahiekpor@dreamoval.com>
2024-02-29 13:41:50 +0000

    Testing 4

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -103,15 +103,13 @@ const Campaign = ({ adminData, campaigns }) => {
       console.log("Unable to copy:", error);
     }
   };
-  const handleShare = async (momoShare,onlineShare, imageUrl) => {
+  const handleShare = async (momoShare, imageUrl) => {
     try {
       if (navigator.share) {
         await navigator.share({
           title: "Agape House New Testament Church",
-          text: "Hi there! It's Claud from Agape House New Testament Church. 🌟 We're on a mission to enhance our sanctuary through Capital Raise 2.0, creating more space for our growing family, from Kidz to Teens. Your support would mean so much to us. If you're able to contribute, every bit helps us move closer to our goal. 🙏 Thank you for considering supporting this cause. Together, we can make a big difference. God bless! 🕊️",
-          files: imageUrl,
-          momoLink: `Mobile Money (MTN Momo, Vodafone Cash, Airtel Tigo) :${momoShare}`,
-          onlineLink: `Mobile Money (MTN Momo, Vodafone Cash, Airtel Tigo) :${onlineShare}`,
+          text: `Hi there! It's Claud from Agape House New Testament Church. 🌟 We're on a mission to enhance our sanctuary through Capital Raise 2.0, creating more space for our growing family, from Kidz to Teens. Your support would mean so much to us. If you're able to contribute, every bit helps us move closer to our goal. 🙏 Thank you for considering supporting this cause. Together, we can make a big difference. God bless! 🕊️\n\n${imageUrl}`,
+          url: `Mobile Money (MTN Momo, Vodafone Cash, Airtel Tigo): ${momoShare}`,
         });}
     } catch (error) {
       console.log("Sharing failed:", error);
@@ -229,7 +227,6 @@ const Campaign = ({ adminData, campaigns }) => {
                           variant="outlined"
                           onClick={() =>
                             handleShare(
-                              `https://collections.kowri.app/main/${item.serviceCode}/${adminData.referalCode}#`,
                               `*227*${item.serviceCode}*${adminData.referalCode}#`,
                                <img src={fund} alt="king"/>
                             )
@@ -268,7 +265,6 @@ const Campaign = ({ adminData, campaigns }) => {
                           onClick={() =>
                             handleShare(
                               `https://collections.kowri.app/main/${item.serviceCode}/${adminData.referalCode}#`,
-                              `*227*${item.serviceCode}*${adminData.referalCode}#`,
                                <img src={fund} alt="king"/>
                             )
                           }
