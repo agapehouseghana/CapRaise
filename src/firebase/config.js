cbc16b2dc8f43ed4cf9807cdfb4af18ed5842c75
davidahiekpor <david.ahiekpor@dreamoval.com>
2024-03-22 10:37:56 +0000

    Firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    FIREBASE_API_KEY: "AIzaSyBnio-PSFk8TkXgcNgC-g7351yD4NubFp4",
-    FIREBASE_AUTH_DOMAIN: "capraise100.firebaseapp.com",
-    FIREBASE_PROJECT_ID: "capraise100",
-    FIREBASE_STORAGE_BUCKET: "capraise100.appspot.com",
-    FIREBASE_MESSAGING_SENDER_ID: "680397788487",
-    FIREBASE_APP_ID: "1:680397788487:web:efc01a5f2776aaaabba862"
+    FIREBASE_API_KEY: "AIzaSyC_R5mFDaNGS6BYdP536mdTAFUno9gFDl0",
+    FIREBASE_AUTH_DOMAIN: "capraise-43d7f.firebaseapp.com",
+    FIREBASE_PROJECT_ID: "capraise-43d7f",
+    FIREBASE_STORAGE_BUCKET: "capraise-43d7f.appspot.com",
+    FIREBASE_MESSAGING_SENDER_ID: "1000835735152",
+    FIREBASE_APP_ID: "1:1000835735152:web:d71de51fe19e6fc4df5eb5"
   };
   
